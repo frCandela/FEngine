210bb6d8ac6a0c5023353ba869a794e8e62f2e45
Candela François <candela.francois@gmail.com>
2019-07-09 08:46:00 +0200

    another tmp

--- FEngine/code/fanEngine.cpp
+++ FEngine/code/fanEngine.cpp
@@ -94,6 +94,49 @@ namespace fan {
 	//================================================================================================================================
 	void Engine::Run()
 	{
+
+		btDefaultCollisionConfiguration *collisionConfiguration = new btDefaultCollisionConfiguration();
+		btCollisionDispatcher *dispatcher = new	btCollisionDispatcher(collisionConfiguration);
+		btBroadphaseInterface *broadphase = new btDbvtBroadphase();
+		btSequentialImpulseConstraintSolver *sequentialImpulseConstraintSolver = new btSequentialImpulseConstraintSolver;
+		btDiscreteDynamicsWorld *world = new btDiscreteDynamicsWorld(dispatcher, broadphase, sequentialImpulseConstraintSolver, collisionConfiguration);
+
+		world->setGravity(btVector3(0, -10, 0));
+
+/*		btCollisionShape* shape = new btBoxShape(btVector3(0.5, 0.5, 0.5));*/
+// 
+// 		btTransform transform;
+// 		transform.setIdentity();
+// 		transform.setOrigin(btVector3(0, 20, 0));
+// 		btVector3 localInertia(0, 0, 0);
+// 		btScalar mass = 0.5f;
+// 		if (mass != 0 ) {
+// 			shape->calculateLocalInertia(mass, localInertia);
+// 		}
+// 		btDefaultMotionState * motionState;
+// 		motionState = new btDefaultMotionState(transform);
+// 
+// 		btRigidBody::btRigidBodyConstructionInfo boxRigidBodyConstructionInfo(mass, motionState, shape, localInertia);
+// 		btRigidBody * body = new btRigidBody(boxRigidBodyConstructionInfo);
+// 		world->addRigidBody(body);
+// 		(void)body;
+
+		//btCollisionShape* shapeSol = new btBoxShape(btVector3(10, 1, 10));	
+		btVector3 normal(0, 1, 0);
+		btCollisionShape* shapeSol = new btStaticPlaneShape(normal, 1.f);
+		btVector3 localInertiaSol(0, 0, 0);
+		btScalar mass = 0;
+		btTransform transformSol;
+		transformSol.setIdentity();
+		transformSol.setOrigin(btVector3(0, -1, 0));
+		btDefaultMotionState * motionStateSol = new btDefaultMotionState(transformSol);
+		btRigidBody::btRigidBodyConstructionInfo solInfo(mass, motionStateSol, shapeSol, localInertiaSol);
+		btRigidBody * bodySol = new btRigidBody(solInfo);
+		(void)bodySol;
+		world->addRigidBody(bodySol);
+
+		const float physicsDelta = 0.02f;
+		float physicsTime = 0.f;
 		float lastUpdateTime = Time::ElapsedSinceStartup();
 		while ( m_applicationShouldExit == false)
 		{
@@ -106,12 +149,61 @@ namespace fan {
 
 			if (updateDelta > 1.f / Time::GetFPS()) {
 				lastUpdateTime = time;
+				physicsTime += updateDelta;
+				
+				
+				while (physicsTime > updateDelta) {
+					physicsTime -= updateDelta;
+					//world->stepSimulation(updateDelta);
+				}
 
 				ActorStart();
 
 				for( scene::Actor * actor : m_activeActors ) {
 					actor->Update(updateDelta);
 				}
+
+
+				{
+
+
+// 					btTransform trans;
+// 					body->getMotionState()->getWorldTransform(trans);
+// 					btVector3 origin = trans.getOrigin();
+// 					float arr[3] = { origin.x(),origin.y(),origin.z() };
+// 					ImGui::DragFloat3("toto", arr);
+
+					static btVector3 from(0,10,0);
+					float arr0[3] = { from.x(),from.y(),from.z() };
+					if (ImGui::DragFloat3("from", arr0)) {
+						from = btVector3(arr0[0], arr0[1], arr0[2]);
+					}
+
+
+					static btVector3 to(0,-10, 0);
+					float arr2[3] = { to.x(),to.y(),to.z() };
+					if (ImGui::DragFloat3("to", arr2)) {
+						to = btVector3(arr2[0], arr2[1], arr2[2]);
+					}
+
+					btCollisionWorld::ClosestRayResultCallback callback(from, to);
+					world->rayTest(from, to, callback);
+					m_renderer->DebugLine(glm::vec3(from.x(), from.y(), from.z()), glm::vec3(to.x(), to.y(), to.z()), glm::vec4(0, 1, 0, 1));
+					bool result = callback.hasHit();
+					ImGui::Checkbox("hit", &result);
+					if (result) {
+						const btVector3 pt = callback.m_hitPointWorld;
+						glm::vec3 ptgl(pt.x(), pt.y(), pt.z());
+						glm::vec4 color(1, 0, 0, 1);
+						m_renderer->DebugLine(ptgl- glm::vec3(0.f, 0.05f, 0.f), ptgl + glm::vec3(0.f, 0.05f, 0.f), color);
+						m_renderer->DebugLine(ptgl - glm::vec3( 0.05f, 0.f, 0.f), ptgl + glm::vec3(0.05f, 0.f, 0.f), color);
+						m_renderer->DebugLine(ptgl - glm::vec3(0.f, 0.f, 0.05f), ptgl + glm::vec3(0.f, 0.f, 0.05f), color);
+
+					}
+				}
+
+
+
 
 				DrawUI();
 				DrawEditorGrid();
@@ -181,44 +273,6 @@ namespace fan {
 	//================================================================================================================================
 	//================================================================================================================================
 	void Engine::DrawUI() {
-// 		static btBroadphaseInterface *myBroadphase = new btDbvtBroadphase();
-// 		static btDefaultCollisionConfiguration *myCollisionConfiguration = new btDefaultCollisionConfiguration();
-// 		static btCollisionDispatcher *myDispatcher = new	btCollisionDispatcher(myCollisionConfiguration);
-// 		static btSequentialImpulseConstraintSolver *mySequentialImpulseConstraintSolver = new btSequentialImpulseConstraintSolver();
-// 		static btDiscreteDynamicsWorld *myWorld = new btDiscreteDynamicsWorld(myDispatcher, myBroadphase, mySequentialImpulseConstraintSolver, myCollisionConfiguration);
-// 		myWorld->setGravity(btVector3(0, -10, 0));
-// 		btTransform myTransform;
-// 		myTransform.setIdentity();
-// 		myTransform.setOrigin(btVector3(0, 0, 0));
-// 
-// 		btVector3 planeDir(btVector3(1, 0, 0));
-// 		btStaticPlaneShape plane(planeDir, 1.f);
-// 
-// 		btSphereShape sphere(1.f);
-// 
-// 		btDefaultMotionState myMotionState(myTransform);
-// 		btRigidBody *body = new btRigidBody(0.f, &myMotionState, &sphere, btVector3(0,0,0));
-// 
-// 		myWorld->addRigidBody(body);
-// 		myWorld->updateAabbs();
-// 		myWorld->computeOverlappingPairs();
-// 
-// 		btVector3 from(0, 2, 0);
-// 		btVector3 to(0, 3, 0);
-// 
-// 		btCollisionWorld::ClosestRayResultCallback  callback(from, to);
-// 		myWorld->rayTest(from, to, callback);
-// 
-// 		bool val = callback.hasHit();
-// 		ImGui::Begin("zob");
-// 		ImGui::Checkbox("ray hit", &val);
-// 		ImGui::End();
-// 
-// 		m_renderer->DebugLine({ from.x(), from.y() , from.z() }, { to.x(), to.y() , to.z() }, { 1,0,0,1 });
-// 
-// 
-// 		myWorld->removeRigidBody(body);
-
 		m_mainMenuBar->Draw();
 		m_renderWindow->Draw();
 		m_sceneWindow->Draw();	
