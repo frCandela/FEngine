2f82d7b8bb7a59f833f00ff20378c6376c86df04
frouty <candela.francois@gmail.com>
2019-09-06 17:39:57 +0200

    dirty wip

---
+++ FEngine/code/core/input/fanKeyboard.h
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace fan {
+	//================================================================================================================================
+	//================================================================================================================================
+	class Keyboard
+	{
+		friend class Input;
+
+	public:
+		static int	IsKeyDown(int _GLFW_KEY) { return glfwGetKey(Input::GetWindow(), _GLFW_KEY) == GLFW_PRESS; }
+		static bool IsKeyPressed(int _GLFW_KEY) { return m_keysPressed[_GLFW_KEY] == Input::GetFrameCount(); }
+		static bool IsKeyReleased(int _GLFW_KEY) { return m_keysReleased[_GLFW_KEY] == Input::GetFrameCount(); }
+
+		enum AzertyKey {
+			D = GLFW_KEY_D,
+			Q = GLFW_KEY_A,
+			S = GLFW_KEY_S,
+			Z = GLFW_KEY_W,
+			A = GLFW_KEY_Q,
+			E = GLFW_KEY_E
+		};
+
+	private:
+		static void KeyCallback(GLFWwindow* _window, int _key, int _scancode, int _action, int _mods);
+		static void CharCallback(GLFWwindow* _window, unsigned int _c);
+
+		//Events
+		static std::array< unsigned, 349 > m_keysPressed;
+		static std::array< unsigned, 349 > m_keysReleased;
+	};
+}
---
+++ FEngine/code/core/input/fanMouse.h
@@ -0,0 +1,43 @@
+#pragma once
+
+namespace fan {
+//================================================================================================================================
+//================================================================================================================================
+	class Mouse
+	{
+		friend class Input;
+	public:
+		enum CursorState { disabled = GLFW_CURSOR_DISABLED, hidden = GLFW_CURSOR_HIDDEN, normal = GLFW_CURSOR_NORMAL };
+		enum Button { button0 = 0, button1 = 1, button2, button3, button4, button5, button6, button7 };
+
+		static btVector2	GetPosition() { return m_position; }
+		static btVector2	GetDelta() { return m_delta; }
+		static btVector2	GetDeltaScroll() { return m_deltaScroll; }
+		static btVector2	GetScreenSpacePosition();
+
+		static void SetCursor(CursorState _state) { glfwSetInputMode(Input::GetWindow(), GLFW_CURSOR, _state); }
+		static void LockCursor(bool _state, btVector2  _position = m_position);
+
+		static bool GetButtonDown(int  _GLFW_MOUSE_BUTTON) { return glfwGetMouseButton(Input::GetWindow(), _GLFW_MOUSE_BUTTON) == GLFW_PRESS; }
+		static bool GetButtonPressed(int _GLFW_MOUSE_BUTTON) { return m_buttonsPressed[_GLFW_MOUSE_BUTTON] == Input::GetFrameCount(); }
+		static bool GetButtonReleased(int _GLFW_MOUSE_BUTTON) { return m_buttonsReleased[_GLFW_MOUSE_BUTTON] == Input::GetFrameCount(); }
+
+	private:
+		static void MouseCallback(GLFWwindow* window, double x, double y) { (void)window;	(void)x;	(void)y; }
+		static void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+		static void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+
+		static void Update();
+
+		static bool			m_lockCursor;
+		static btVector2	m_lockPosition;
+
+		static btVector2	m_oldPosition;
+		static btVector2	m_position;
+		static btVector2	m_delta;
+		static btVector2	m_deltaScroll;
+
+		static std::array< unsigned, 11 > m_buttonsPressed;
+		static std::array< unsigned, 11 > m_buttonsReleased;
+	};
+}
