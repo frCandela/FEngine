5d2a511deb271a48d416bcd226b36d7402e797b9
frcandela <candela.francois@gmail.com>
2019-09-21 15:33:45 +0200

    Better signal class without lambas functions

--- FEngine/code/core/fanSignal.h
+++ FEngine/code/core/fanSignal.h
@@ -1,64 +1,73 @@
 #pragma once
 
 #include <vector>
-#include <functional>
 
 namespace fan {
 	//================================================================================================================================
 	//================================================================================================================================
-	template < typename... T > class Signal
-	{
+	template < typename... T > class Signal {
+	private:
+		//================================================================
+		struct SlotBase {
+			virtual void Call(T... _args) = 0;
+		};
+
+		//================================================================
+		template <typename ClassType>
+		class Slot : public SlotBase {
+		public:
+			Slot(void(ClassType::* _method)(T...), ClassType * _object) :
+				m_object(_object),
+				m_method(_method) {}
+
+			void Call(T... _args) override { ((*m_object).*(m_method))(_args...); }
+
+			bool Equals(void(ClassType::* const _method)(T...), const ClassType * _object) {
+				return m_object == _object && m_method == _method;
+			}
+		private:
+			ClassType * m_object;
+			void (ClassType::*m_method)(T...);
+		};
+
 	public:
-		void Emmit(T... args)
-		{
-			for (auto slot : m_slots)
-				slot(args...);
+		~Signal() {
+			for (size_t slotIndex = 0; slotIndex < m_slots.size(); slotIndex++) {
+				delete(m_slots[slotIndex]);
+			}
+			m_slots.clear();
 		}
 
-		template <typename ClassA, typename ClassB>
-		//!< Connect the Signal to the method dst of  class
-		void Connect(void(ClassA::* slot)(T...), ClassB* dst)
-		{
-			auto s = std::function<void(T...)>(
-				[=](T... args)
-			{
-				return (dst->*slot)(args...);
+		// Connects the signal to the member function _method of ClassType _object
+		template <typename ClassType>
+		void Connect(void(ClassType::* _method)(T...), ClassType * _object) {
+			m_slots.push_back(new Slot<ClassType>(_method, _object));
+		}
+
+		// Disconnect the signal from the the member function _method of ClassType _object
+		template <typename ClassType>
+		void Disconnect(void(ClassType::* _method)(T...), ClassType* _object) {
+			for (size_t slotIndex = 0; slotIndex < m_slots.size(); slotIndex++) {
+				SlotBase * slotbase = m_slots[slotIndex];
+				Slot<ClassType> * slot = static_cast<Slot<ClassType> *>(slotbase);
+
+				if (slot->Equals(_method, _object)) {
+					delete(slot);
+					m_slots.erase(m_slots.begin() + slotIndex);
+					return;
+				}
 			}
-			);
-			m_slots.push_back(s);
+		}
+
+		// Calls all slots connected to the signal
+		void Emmit(T... _args) {
+			for (size_t slotIndex = 0; slotIndex < m_slots.size(); slotIndex++) {
+				SlotBase * slot = m_slots[slotIndex];
+				slot->Call(_args...);
+			}
 		}
 
 	private:
-		std::vector< std::function<void(T...)> > m_slots; //!< Vector of slots (functions) called when the signal is emmited
-	};
-
-
-	// No argument version of the class Signal
-	template <> class Signal<void>
-	{
-	public:
-		//!< \brief Emmit a signal = call the slots (functions) of the receiver.
-		void Emmit()
-		{
-			for (auto slot : m_slots)
-				slot();
-		}
-
-
-		template <typename ClassA, typename ClassB>
-		//!< Connect 2 widgets with a signal. \param slot adress of the slot (fonction) in the receiver. \param dst adress of the receiver.
-		void Connect(void(ClassA::* slot)(), ClassB* dst)
-		{
-			auto s = std::function<void()>(
-				[=]()
-			{
-				return (dst->*slot)();
-			}
-			);
-			m_slots.push_back(s);
-		}
-
-	private:
-		std::vector< std::function<void()> > m_slots{}; //!< Vector of slots (functions) called when the signal is emmited
+		std::vector<SlotBase*> m_slots;
 	};
 }
