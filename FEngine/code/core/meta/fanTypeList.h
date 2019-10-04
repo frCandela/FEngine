ac0fef6f2d49288fcec41b61f33a3e85c7b4855c
frcandela <candela.francois@gmail.com>
2019-10-04 08:29:53 +0200

    Meta lib : typelist & extractor

---
+++ FEngine/code/core/meta/fanTypeList.h
@@ -0,0 +1,37 @@
+#pragma once
+
+namespace meta {
+	//================================================================================================================================
+	// Type List 
+	//================================================================================================================================
+	struct NullType;
+
+	// General type list 
+	template< typename _THead, typename ... _TTypes >
+	struct TypeList {
+		using head = _THead;
+		using tail = typename TypeList< _TTypes... >;
+	};
+
+	// Type list specialization with a single element
+	template< typename _THead >
+	struct TypeList< _THead > {
+		using head = _THead;
+		using tail = NullType;
+	};
+
+
+	//================================================================================================================================
+	// Type list extractor
+	//================================================================================================================================
+	template< size_t _Index, typename _TypeList  >
+	struct Extract {
+		using result = typename Extract<_Index - 1, typename _TypeList::tail >::result;
+	};
+
+	template< typename _TypeList  >
+	struct Extract<0, _TypeList > {
+		using result = typename _TypeList::head;
+	};
+
+}
