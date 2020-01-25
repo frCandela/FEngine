f59e0d611432ad66f93d17cf64934bf172b6654f
frouty <candela.francois@gmail.com>
2020-01-25 11:31:32 +0100

    cleaning files

---
+++ code/editor/windows/fanConsoleWindow.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "editor/windows/fanEditorWindow.h"
+
+namespace fan
+{
+	//================================================================================================================================
+	//================================================================================================================================
+	class ConsoleWindow : public EditorWindow {
+
+	private:
+		// Fast drawing version
+		struct LogItem {
+			LogItem(const Debug::LogItem& _logItem);
+			Debug::Severity	severity;
+			Debug::Type		logType;
+			std::string		logMessage;
+			ImVec4			color;
+		};
+
+	public:
+		ConsoleWindow();
+		~ConsoleWindow() override;
+
+	protected:
+		void OnGui() override;
+
+	private:
+		static const uint32_t s_inputBufferSize = 256;
+
+		std::vector<LogItem> m_logBuffer;
+		int		m_maxSizeLogBuffers;					// Number of logs kept in memory		
+		int		m_firstLogIndex;						// m_logBuffer is used as a circular array
+		char	m_inputBuffer[s_inputBufferSize];
+		bool	m_scrollDown;
+		bool	m_grabFocus;
+
+		static ImVec4	GetSeverityColor(const Debug::Severity & _severity);
+		void			OnNewLog(Debug::LogItem _item);
+	};
+}
