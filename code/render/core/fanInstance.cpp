e9f67c5d96643712b7be972200dc75909bf18b7c
frouty <candela.francois@gmail.com>
2020-02-29 07:44:46 +0100

    more validation layers

--- code/render/core/fanInstance.cpp
+++ code/render/core/fanInstance.cpp
@@ -25,20 +25,18 @@ namespace fan
 		SetDesiredExtensions( desiredExtensions );
 
 
-	#ifdef NDEBUG
-		const std::vector<const char*> validationLayers = {};
-	#else	
+	#ifndef NDEBUG
 		SetDesiredValidationLayers( {
 			 "VK_LAYER_LUNARG_standard_validation"
-			 //,"VK_LAYER_LUNARG_assistant_layer"
+			 ,"VK_LAYER_LUNARG_assistant_layer"
 			 ,"VK_LAYER_LUNARG_core_validation"
-									/*,"VK_LAYER_KHRONOS_validation"
-									,"VK_LAYER_LUNARG_monitor"
-									,"VK_LAYER_LUNARG_object_tracker"
-									,"VK_LAYER_LUNARG_screenshot"
-									,"VK_LAYER_LUNARG_standard_validation"
-									,"VK_LAYER_LUNARG_parameter_validation"*/
-									} );
+			,"VK_LAYER_KHRONOS_validation"
+			,"VK_LAYER_LUNARG_monitor"
+			,"VK_LAYER_LUNARG_object_tracker"
+			,"VK_LAYER_LUNARG_screenshot"
+			,"VK_LAYER_LUNARG_standard_validation"
+			,"VK_LAYER_LUNARG_parameter_validation"
+			} );
 	#endif
 
 		VkApplicationInfo appInfo;
