ca43f758ea4964baa04a8f40953b61dba2686b7f
frouty <candela.francois@gmail.com>
2020-03-30 09:09:07 +0200

    Signal class cleaning

--- code/core/fanSignal.hpp
+++ code/core/fanSignal.hpp
@@ -4,84 +4,112 @@
 
 namespace fan
 {
-	//================================================================================================================================
-	//================================================================================================================================
-	template < typename... T > class Signal
+	namespace impl
 	{
-	private:
-		//================================================================
+		//================================================================================================================================
+		//================================================================================================================================
+		template < typename... _Args >
 		struct SlotBase
 		{
-			virtual void Call( T... _args ) = 0;
+			virtual void Call( _Args... _args ) = 0;
 		};
 
-		//================================================================
-		template <typename ClassType>
-		class Slot : public SlotBase
+		//================================================================================================================================
+		//================================================================================================================================
+		template <typename _Target, typename... _Args >
+		class Slot : public SlotBase<_Args...>
 		{
 		public:
-			Slot( void( ClassType::* _method )( T... ), ClassType* _object ) :
+			Slot( void( _Target::* _method )( _Args... ), _Target* _object ) :
 				m_object( _object ),
 				m_method( _method )
 			{}
 
-			void Call( T... _args ) override { ( ( *m_object ).*( m_method ) )( _args... ); }
+			void Call( _Args... _args ) override { ( ( *m_object ).*( m_method ) )( _args... ); }
 
-			bool Equals( void( ClassType::* const _method )( T... ), const ClassType* _object )
+			bool Equals( void( _Target::* const _method )( _Args... ), const _Target* _object )
 			{
 				return m_object == _object && m_method == _method;
 			}
 		private:
-			ClassType* m_object;
-			void ( ClassType::* m_method )( T... );
+			_Target* m_object;
+			void ( _Target::* m_method )( _Args... );
 		};
+	}
 
+	//================================================================================================================================
+	// Signal
+	//================================================================================================================================
+	template < typename... _Args > class Signal
+	{
 	public:
-		~Signal()
-		{
-			for ( size_t slotIndex = 0; slotIndex < m_slots.size(); slotIndex++ )
-			{
-				delete( m_slots[ slotIndex ] );
-			}
-			m_slots.clear();
-		}
-
-		// Connects the signal to the member function _method of ClassType _object
-		template <typename ClassType>
-		void Connect( void( ClassType::* _method )( T... ), ClassType* _object )
-		{
-			m_slots.push_back( new Slot<ClassType>( _method, _object ) );
-		}
-
-		// Disconnect the signal from the the member function _method of ClassType _object
-		template <typename ClassType>
-		void Disconnect( void( ClassType::* _method )( T... ), ClassType* _object )
-		{
-			for ( size_t slotIndex = 0; slotIndex < m_slots.size(); slotIndex++ )
-			{
-				SlotBase* slotbase = m_slots[ slotIndex ];
-				Slot<ClassType>* slot = static_cast< Slot<ClassType>* >( slotbase );
-
-				if ( slot->Equals( _method, _object ) )
-				{
-					delete( slot );
-					m_slots.erase( m_slots.begin() + slotIndex );
-					return;
-				}
-			}
-		}
-
-		// Calls all slots connected to the signal
-		void Emmit( T... _args )
-		{
-			for ( size_t slotIndex = 0; slotIndex < m_slots.size(); slotIndex++ )
-			{
-				SlotBase* slot = m_slots[ slotIndex ];
-				slot->Call( _args... );
-			}
-		}
+		~Signal();
+		
+		template <typename _Target>
+		void Connect( void( _Target::* _method )( _Args... ), _Target* _object );
+		template <typename _Target>
+		void Disconnect( void( _Target::* _method )( _Args... ), _Target* _object );
+		void Emmit( _Args... _args );
+		void Clear(){ m_slots.clear(); }
 
 	private:
-		std::vector<SlotBase*> m_slots;
+		std::vector<impl::SlotBase< _Args...>*> m_slots;
 	};
+
+	//================================================================================================================================
+	// destructor
+	//================================================================================================================================
+	template < typename...  _Args >
+	Signal< _Args...>::~Signal()
+	{
+		for( size_t slotIndex = 0; slotIndex < m_slots.size(); slotIndex++ )
+		{
+			delete( m_slots[slotIndex] );
+		}
+		m_slots.clear();
+	}
+
+	//================================================================================================================================
+	// calls all slots connected to the signal
+	//================================================================================================================================
+	template < typename...  _Args >
+	void Signal< _Args...>::Emmit( _Args... _args )
+	{
+		for( size_t slotIndex = 0; slotIndex < m_slots.size(); slotIndex++ )
+		{
+			impl::SlotBase< _Args...>* slot = m_slots[slotIndex];
+			slot->Call( _args... );
+		}
+	}
+
+	//================================================================================================================================
+	// disconnect the signal from the the member function _method of _Target object
+	//================================================================================================================================
+	template < typename...  _Args >
+	template <typename _Target>
+	void Signal< _Args...>::Disconnect( void( _Target::* _method )( _Args... ), _Target* _object )
+	{
+		for( size_t slotIndex = 0; slotIndex < m_slots.size(); slotIndex++ )
+		{
+			impl::SlotBase< _Args...>* slotbase = m_slots[slotIndex];
+			impl::Slot<_Target, _Args...>* slot = static_cast<impl::Slot<_Target, _Args...>*>( slotbase );
+
+			if( slot->Equals( _method, _object ) )
+			{
+				delete( slot );
+				m_slots.erase( m_slots.begin() + slotIndex );
+				return;
+			}
+		}
+	}
+
+	//================================================================================================================================
+	// connects the signal to the member function _method of _Target _object
+	//================================================================================================================================
+	template < typename...  _Args >
+	template <typename _Target>
+	void Signal< _Args...>::Connect( void( _Target::* _method )( _Args... ), _Target* _object )
+	{
+		m_slots.push_back( new impl::Slot<_Target, _Args...>( _method, _object ) );
+	}
 }
