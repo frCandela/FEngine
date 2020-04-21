919b8269c08c08eee7289921f10f285b91d2df3f
frouty <candela.francois@gmail.com>
2020-04-21 08:04:24 +0200

    Signal renaming

--- code/core/fanSignal.hpp
+++ code/core/fanSignal.hpp
@@ -31,18 +31,18 @@ namespace fan
 		{
 			std::function<void( _Args... )> func = [_method, _object]( _Args... _args ) { ( ( *_object ).*( _method ) )( _args... ); };
 			Connection c = { _object, func };
-			m_connections.push_back( c );
+			connections.push_back( c );
 		}
 
 		template <typename _Object >
 		void Disconnect( _Object* _object )
 		{
-			for( int i = 0; i < m_connections.size(); i++ )
+			for( int i = 0; i < connections.size(); i++ )
 			{
-				Connection& connection = m_connections[i];
+				Connection& connection = connections[i];
 				if( connection.object )
 				{
-					m_connections.erase( m_connections.begin() + i );
+					connections.erase( connections.begin() + i );
 					return;
 				}
 			}
@@ -50,13 +50,13 @@ namespace fan
 
 		void Emmit( _Args... _args )
 		{
-			for( auto connection : m_connections )
+			for( auto connection : connections )
 			{
 				connection.lambda( _args... );
 			}
 		}
-		void Clear() { m_connections.clear(); }
+		void Clear() { connections.clear(); }
 
-		std::vector< Connection > m_connections;
+		std::vector< Connection > connections;
 	};
 }
