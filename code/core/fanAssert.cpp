6a2cfd4072bcced8f9b5c2151068e6b5c2a5a53b
frouty <candela.francois@gmail.com>
2020-08-22 12:13:39 +0200

    Remove namespace dependency on fanAssert

--- code/core/fanAssert.cpp
+++ code/core/fanAssert.cpp
@@ -1,10 +1,10 @@
 #include "core/fanAssert.hpp"
 #include "core/fanDebug.hpp"
 
+
+#ifndef NDEBUG
 namespace fan
 {
-#ifndef NDEBUG
-
     bool AssertUtils::sFanAssertBreakEnabled = true;
 
     //========================================================================================================
@@ -12,12 +12,11 @@ namespace fan
     void AssertUtils::LogAssertMessage( const char* _msg, const char* _func, int _line, const char* _file )
     {
         Debug::Error() << "Assert:"
-                    << _msg
-                    << " func:" << _func
-                    << " line:" << _line
-                    << " file:" << _file
-                    << Debug::Endl();
+                << _msg
+                << " func:" << _func
+                << " line:" << _line
+                << " file:" << _file
+                << Debug::Endl();
     }
-
-#endif
 }
+#endif
--- code/core/fanAssert.hpp
+++ code/core/fanAssert.hpp
@@ -2,34 +2,31 @@
 
 #include "core/fanSystem.hpp"
 
-namespace fan
-{
-    #ifdef NDEBUG
-
-        #define fanAssertMsg( _expression, _msg )
-        #define fanAssert( _expression )
-
-    #else
-
+#ifdef NDEBUG
+    #define fanAssertMsg( _expression, _msg )
+    #define fanAssert( _expression )
+#else
+    namespace fan
+    {
         struct AssertUtils
         {
             static bool sFanAssertBreakEnabled;
             static void LogAssertMessage( const char* _msg, const char* _func, int _line, const char* _file );
         };
+    }
 
-        #define fanAssertMsg( _expression, _msg )                                       \
-            do                                                                          \
-            {                                                                           \
-                if( ! (_expression) )                                                   \
-                {                                                                       \
-                    if( AssertUtils::sFanAssertBreakEnabled ){ FAN_DEBUG_BREAK }        \
-                    AssertUtils::LogAssertMessage( _msg, __func__, __LINE__, __FILE__ );\
-                }                                                                       \
-            }                                                                           \
-            while( false )
+    #define fanAssertMsg( _expression, _msg )                                               \
+        do                                                                                  \
+        {                                                                                   \
+            if( ! (_expression) )                                                           \
+            {                                                                               \
+                if( fan::AssertUtils::sFanAssertBreakEnabled ){ FAN_DEBUG_BREAK }           \
+                fan::AssertUtils::LogAssertMessage( _msg, __func__, __LINE__, __FILE__ );   \
+            }                                                                               \
+        }                                                                                   \
+        while( false )
 
-        #define fanAssert( _expression ) fanAssertMsg( _expression, "" )
+    #define fanAssert( _expression ) fanAssertMsg( _expression, "" )
+#endif
 
-    #endif
-}
 
