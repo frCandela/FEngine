7714670d9ed4bff61050ea274fc26578fbe67f7e
frouty <candela.francois@gmail.com>
2021-03-07 08:08:16 +0100

    Created new project : voxels

---
+++ projects/project_voxels/code/editor/components/fanGuiTestComponent.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "game/components/fanTestComponent.hpp"
+#include "editor/singletons/fanEditorGuiInfo.hpp"
+
+namespace fan
+{
+    struct GuiTestComponent
+    {
+        //====================================================================================================
+        //====================================================================================================
+        static GuiComponentInfo GetInfo()
+        {
+            GuiComponentInfo info;
+            info.mIcon       = ImGui::IconType::None16;
+            info.mGroup      = EngineGroups::Game;
+            info.onGui       = &GuiTestComponent::OnGui;
+            info.mEditorName = "test component";
+            info.mEditorPath = "gamesss/";
+            return info;
+        }
+
+        //========================================================================================================
+        //========================================================================================================
+        static void OnGui( EcsWorld& /*_world*/, EcsEntity /*_entityID*/, EcsComponent& _component )
+        {
+            TestComponent& testComponent = static_cast<TestComponent&>( _component );
+            ImGui::DragInt( "test value", &testComponent.mValue );
+        }
+    };
+}
---
+++ projects/project_voxels/content/shaders/models.vert
@@ -0,0 +1,34 @@
+#version 450
+
+layout(binding = 0) uniform UniformBufferObject 
+{
+    mat4 view;
+    mat4 proj;
+} ubo;
+
+layout (binding = 1) uniform DynamicUniformBufferObject 
+{
+	mat4 modelMat;
+	mat4 normalMat;
+} dynamicUbo;
+
+layout (location = 0) in vec3 inPosition;
+layout (location = 1) in vec3 inNormal;
+layout (location = 2) in vec3 inColor;
+layout (location = 3) in vec2 inTexCoord;
+
+layout (location = 0) out vec3 outColor;
+layout (location = 1) out vec3 outFragPos;
+layout (location = 2) out vec3 outNormal;
+layout (location = 3) out vec2 outTexCoord;
+
+void main() 
+{
+	const vec4 worldPos = dynamicUbo.modelMat * vec4(inPosition, 1.0);
+	gl_Position = ubo.proj * ubo.view * worldPos;
+
+	outColor = inColor;
+	outFragPos = worldPos.xyz;
+	outNormal =  (dynamicUbo.normalMat * vec4(inNormal,1)).xyz;
+	outTexCoord = inTexCoord;
+}
